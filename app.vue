933149e81a3b25fada96843e6f047eba3b8b212f
skiqq <chibifoxdiscord+github@gmail.com>
2025-05-14 20:20:38 -0300

    Refactor code structure for improved readability and maintainability

--- app.vue
+++ app.vue
@@ -1,66 +1,73 @@
 <template>
-  <Link rel='icon' type='image/png' href='/squaredskwb.svg' />
+
+  <Link rel="icon" type="image/png" href="/squaredskwb.svg" />
   <Title>SKWB :: ☆</Title>
+
   <UApp :toaster="{
     class: 'left-0 *:right-0 *:bottom-0 *:p-2 *:bg-[#11111b] *:border-2 *:hover:border-[#45475a] *:border-[#313244] *:flex *:items-center *:gap-2 *:text-[#fff]',
   }">
-    <div class="flex md:flex-row flex-col h-fit gap-4 w-11/12 md:h-11/12">
-      <div class="flex flex-col h-full gap-4">
-        <div class="container relative h-full">
-          <h1 class="px-2 bg-[#11111b] absolute -top-4">Site map</h1>
-          <Tree :items="pages" />
+    <div class="flex md:flex-row overflow-auto md:mt-0 mt-8 flex-col gap-4 w-11/12">
+      <div class="flex flex-col gap-4 md:w-3/12 w-full md:sticky md:top-0 md:self-start h-fit">
+        <div class="flex flex-col -space-y-3">
+          <button @click="toggleSiteMap" class="flex items-center gap-1 z-10 bg-[#11111b] px-2 w-fit ml-4">
+            <Icon name="mdi:chevron-right" class="transition-transform duration-200 ease-in-out"
+              :class="{ 'rotate-90': isSiteMapOpen }" />
+            Site map
+          </button>
+          <div class="container">
+            <Tree v-if="isSiteMapOpen" :items="pages" />
+          </div>
         </div>
-        <div class="container relative h-full">
-          <h1 class="px-2 bg-[#11111b] absolute -top-4">The socials</h1>
-          <Tree :items="social" />
+        <div class="flex flex-col -space-y-3">
+          <button @click="toggleSocials" class="flex items-center gap-1 z-10 bg-[#11111b] px-2 w-fit ml-4">
+            <Icon name="mdi:chevron-right" class="transition-transform duration-200 ease-in-out"
+              :class="{ 'rotate-90': isSocialsOpen }" />
+            The socials
+          </button>
+          <div class="container">
+            <Tree v-if="isSocialsOpen" :items="social" />
+          </div>
         </div>
       </div>
-      <div class="container relative">
-        <h1 class="px-2 bg-[#11111b] absolute -top-4">SKWB</h1>
-        <NuxtPage />
+      <div class="flex flex-col w-full -space-y-3">
+        <button class="flex items-center gap-1 z-10 bg-[#11111b] w-fit ml-4 px-2 ">
+          SKWB
+        </button>
+        <div class="container w-full">
+          <NuxtPage />
+        </div>
       </div>
     </div>
+
   </UApp>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const isSiteMapOpen = ref(true);
+const isSocialsOpen = ref(true);
+
+const toggleSiteMap = () => {
+  isSiteMapOpen.value = !isSiteMapOpen.value;
+};
+
+const toggleSocials = () => {
+  isSocialsOpen.value = !isSocialsOpen.value;
+};
 
 const pages = [
-  {
-    icon: 'mdi:home',
-    name: 'Home',
-    link: '/',
-  },
-  {
-    icon: 'material-symbols:person-book',
-    name: 'Characters',
-    link: '/characters',
-  },
-  {
-    icon: 'material-symbols:newspaper',
-    name: 'Blog',
-    link: '/blog',
-  },
-  {
-    icon: 'material-symbols:question-mark',
-    name: 'Facts',
-    link: '/facts',
-  },
+  { icon: 'mdi:home', name: 'Home', link: '/' },
+  { icon: 'material-symbols:person-book', name: 'Characters', link: '/characters' },
+  { icon: 'material-symbols:newspaper', name: 'Blog', link: '/blog' },
+  { icon: 'material-symbols:code', name: 'Projects', link: '/projects' },
   {
     icon: 'maki:museum',
     name: 'Museum',
     link: '/museum',
     children: [
-      {
-        icon: 'mdi:palette',
-        name: 'Drawings',
-        link: '/drawings',
-      },
-      {
-        icon: 'material-symbols:music-note',
-        name: 'Musics',
-        link: '/musics',
-      }
+      { icon: 'mdi:palette', name: 'Drawings', link: '/drawings' },
+      { icon: 'material-symbols:music-note', name: 'Musics', link: '/musics' },
     ],
   },
   {
@@ -68,50 +75,18 @@ const pages = [
     name: 'Members',
     link: '/members',
     children: [
-      {
-        icon: 'game-icons:goat',
-        name: 'Skiqq',
-        link: '/members/skiqq',
-      },
-      {
-        icon: 'material-symbols:add',
-        name: 'Join',
-        link: '/members/how2join',
-      }
+      { icon: 'game-icons:goat', name: 'Skiqq', link: '/members/skiqq' },
+      { icon: 'material-symbols:add', name: 'Join', link: '/members/how2join' },
     ],
   },
-]
+];
 
 const social = [
-  {
-    icon: 'material-symbols:planet',
-    name: 'Source code',
-    link: 'https://github.com/SKWB-gh/skwb.website',
-  },
-  {
-    icon: 'carbon:logo-x',
-    name: 'X/Twitter',
-    link: 'https://x.com/skwb_hq',
-  },
-  {
-    icon: 'mdi:instagram',
-    name: 'Instagram',
-    link: 'https://www.instagram.com/skwb.hq',
-  },
-  {
-    icon: 'ic:baseline-discord',
-    name: 'Discord',
-    link: 'https://discord.gg/TAfgTxjDxM',
-  },
-  {
-    icon: 'ic:baseline-telegram',
-    name: 'Telegram',
-    link: 'https://t.me/skwb_hq',
-  },
-  {
-    icon: 'mdi:rss',
-    name: 'RSS',
-    link: '/blog/rss',
-  }
-]
+  { icon: 'material-symbols:planet', name: 'Source code', link: 'https://github.com/skiqq-flower/skwb.website' },
+  { icon: 'carbon:logo-x', name: 'X/Twitter', link: 'https://x.com/skwb_hq' },
+  { icon: 'mdi:instagram', name: 'Instagram', link: 'https://www.instagram.com/skwb.hq' },
+  { icon: 'ic:baseline-discord', name: 'Discord', link: 'https://discord.gg/TAfgTxjDxM' },
+  { icon: 'ic:baseline-telegram', name: 'Telegram', link: 'https://t.me/skwb_hq' },
+  { icon: 'mdi:rss', name: 'RSS', link: '/blog/rss' },
+];
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -7,14 +7,59 @@
     <p>SKWB is a hub for creative content, we aim to create online media, such as comics, drawings, musics, games and so
         forth.</p>
     <h2 class="title mt-4">Learn more with our pages</h2>
-    <div class="flex md:flex-row flex-col h-fit gap-4 w-11/12">
-        <div class="hover:border-[#45475a] border-2 p-2 w-56 flex flex-col border-[#313244]">
+    <div class="flex md:flex-row md:flex-wrap flex-col h-fit gap-4 md:w-11/12 md:items-start items-center w-full">
+        <div class="hover:border-[#45475a] border-2 p-2 w-fit flex flex-col border-[#313244]">
             <img src="/bodedope_pfp.png" class="size-56">
             <p class="text-xl font-bold font-mono">Bodedope</p>
-            <p>One of our characters, a lovely goat filled with emotions.</p>
+            <p class="w-56">One of our characters, a lovely goat filled with emotions.</p>
             <NuxtLink to="/characters/bodedope"
                 class="border-[#313244] hover:border-[#45475a] mt-2 border-2 flex flex-row items-center">
                 <Icon name="mdi:chevron-right" /> See all characters
+            </NuxtLink>
+        </div>
+        <div class="hover:border-[#45475a] border-2 p-2 w-fit flex flex-col border-[#313244]">
+            <img src="/epnoia_pfp.png" class="size-56">
+            <p class="text-xl font-bold font-mono">Epnoia</p>
+            <p class="w-56">Our first game, yet in development.</p>
+            <NuxtLink to="/characters/bodedope"
+                class="border-[#313244] hover:border-[#45475a] mt-2 border-2 px-2 flex flex-row items-center">
+                <Icon name="mdi:chevron-right" /> Check out our projects
+            </NuxtLink>
+        </div>
+        <div class="hover:border-[#45475a] border-2 p-2 w-fit flex flex-col border-[#313244]">
+            <Icon name="material-symbols:newspaper" class="size-56" />
+            <p class="text-xl font-bold font-mono">What we are doing?</p>
+            <p class="w-56">You can check the blog for updates.</p>
+            <NuxtLink to="/characters/bodedope"
+                class="border-[#313244] hover:border-[#45475a] mt-2 border-2 px-2 flex flex-row items-center">
+                <Icon name="mdi:chevron-right" /> Visit our blog
+            </NuxtLink>
+        </div>
+        <div class="hover:border-[#45475a] border-2 p-2 w-fit flex flex-col border-[#313244]">
+            <Icon name="maki:museum" class="size-56" />
+            <p class="text-xl font-bold font-mono">The SKWB museum</p>
+            <p class="w-56">Drawings and Music, a museum that is open 24/7.</p>
+            <NuxtLink to="/characters/bodedope"
+                class="border-[#313244] hover:border-[#45475a] mt-2 border-2 px-2 flex flex-row items-center">
+                <Icon name="mdi:chevron-right" /> Have fun in museum
+            </NuxtLink>
+        </div>
+        <div class="hover:border-[#45475a] border-2 p-2 w-fit flex flex-col border-[#313244]">
+            <Icon name="maki:museum" class="size-56" />
+            <p class="text-xl font-bold font-mono">The SKWB museum</p>
+            <p class="w-56">Drawings and Music, a museum that is open 24/7.</p>
+            <NuxtLink to="/characters/bodedope"
+                class="border-[#313244] hover:border-[#45475a] mt-2 border-2 px-2 flex flex-row items-center">
+                <Icon name="mdi:chevron-right" /> Have fun in museum
+            </NuxtLink>
+        </div>
+        <div class="hover:border-[#45475a] border-2 p-2 w-fit flex flex-col border-[#313244]">
+            <Icon name="material-symbols:groups" class="size-56" />
+            <p class="text-xl font-bold font-mono">All members</p>
+            <p class="w-56">Come see the members of SKWB (you can be one too!)</p>
+            <NuxtLink to="/characters/bodedope"
+                class="border-[#313244] hover:border-[#45475a] mt-2 border-2 px-2 flex flex-row items-center">
+                <Icon name="mdi:chevron-right" /> Check the members
             </NuxtLink>
         </div>
     </div>
