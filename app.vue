856bf097b0432281924be3a9f69e30b5bcba1547
skiqq <chibifoxdiscord+github@gmail.com>
2025-05-13 21:41:22 -0300

    * Updated main page with more information * Updated social media links

--- app.vue
+++ app.vue
@@ -1,9 +1,10 @@
 <template>
+  <Link rel='icon' type='image/png' href='/squaredskwb.svg' />
   <Title>SKWB :: ☆</Title>
   <UApp :toaster="{
     class: 'left-0 *:right-0 *:bottom-0 *:p-2 *:bg-[#11111b] *:border-2 *:hover:border-[#45475a] *:border-[#313244] *:flex *:items-center *:gap-2 *:text-[#fff]',
   }">
-    <div class="flex md:flex-row flex-col h-fit gap-2 w-11/12 md:h-11/12">
+    <div class="flex md:flex-row flex-col h-fit gap-4 w-11/12 md:h-11/12">
       <div class="flex flex-col h-full gap-4">
         <div class="container relative h-full">
           <h1 class="px-2 bg-[#11111b] absolute -top-4">Site map</h1>
@@ -42,8 +43,8 @@ const pages = [
   },
   {
     icon: 'material-symbols:question-mark',
-    name: 'FAQ',
-    link: '/blog',
+    name: 'Facts',
+    link: '/facts',
   },
   {
     icon: 'maki:museum',
@@ -62,28 +63,50 @@ const pages = [
       }
     ],
   },
+  {
+    icon: 'material-symbols:groups',
+    name: 'Members',
+    link: '/members',
+    children: [
+      {
+        icon: 'game-icons:goat',
+        name: 'Skiqq',
+        link: '/members/skiqq',
+      },
+      {
+        icon: 'material-symbols:add',
+        name: 'Join',
+        link: '/members/how2join',
+      }
+    ],
+  },
 ]
 
 const social = [
   {
-    icon: 'mdi:github',
-    name: 'GitHub',
-    link: 'https://github.com/SKIQQ-flower',
+    icon: 'material-symbols:planet',
+    name: 'Source code',
+    link: 'https://github.com/SKWB-gh/skwb.website',
   },
   {
-    icon: 'mdi:twitter',
-    name: 'Twitter',
-    link: 'https://twitter.com/SKIQQ_',
+    icon: 'carbon:logo-x',
+    name: 'X/Twitter',
+    link: 'https://x.com/skwb_hq',
   },
   {
     icon: 'mdi:instagram',
     name: 'Instagram',
-    link: 'https://www.instagram.com/skwb.art',
+    link: 'https://www.instagram.com/skwb.hq',
   },
   {
     icon: 'ic:baseline-discord',
     name: 'Discord',
-    handle: '@skiqq'
+    link: 'https://discord.gg/TAfgTxjDxM',
+  },
+  {
+    icon: 'ic:baseline-telegram',
+    name: 'Telegram',
+    link: 'https://t.me/skwb_hq',
   },
   {
     icon: 'mdi:rss',
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,21 @@
 <template>
+    <img class="border-2 p-2 hover:border-[#45475a] border-[#313244]" src="/skwbtext.svg">
     <h1 class="title">Greetings, this is SKWB!</h1>
-    <p>Hello, im Skiqq, and this is SKWB, the "everything related to me" website.</p>
+    <p>Hello, welcome to SKWB website, the heart of our company. Check the projects, social medias, blog and other pages
+        made with &lt;3</p>
+    <h2 class="title mt-4">About</h2>
+    <p>SKWB is a hub for creative content, we aim to create online media, such as comics, drawings, musics, games and so
+        forth.</p>
+    <h2 class="title mt-4">Learn more with our pages</h2>
+    <div class="flex md:flex-row flex-col h-fit gap-4 w-11/12">
+        <div class="hover:border-[#45475a] border-2 p-2 w-56 flex flex-col border-[#313244]">
+            <img src="/bodedope_pfp.png" class="size-56">
+            <p class="text-xl font-bold font-mono">Bodedope</p>
+            <p>One of our characters, a lovely goat filled with emotions.</p>
+            <NuxtLink to="/characters/bodedope"
+                class="border-[#313244] hover:border-[#45475a] mt-2 border-2 flex flex-row items-center">
+                <Icon name="mdi:chevron-right" /> See all characters
+            </NuxtLink>
+        </div>
+    </div>
 </template>
